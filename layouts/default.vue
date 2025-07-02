3af9f2afa5f1d554627726c84a73b98152dc1281
kkaaiiww <67319100007@pyt.ac.th>
2025-07-02 11:51:32 +0700

    my first project

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,6 +6,7 @@
       :clipped="clipped"
       fixed
       app
+      class="elevation-2"
     >
       <v-list>
         <v-list-item
@@ -24,65 +25,58 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
+
     <v-app-bar
       :clipped-left="clipped"
       fixed
       app
+      color="primary"
+      dark
+      class="elevation-4"
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
         <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
+      <v-btn icon @click.stop="clipped = !clipped">
         <v-icon>mdi-application</v-icon>
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
+      <v-btn icon @click.stop="fixed = !fixed">
         <v-icon>mdi-minus</v-icon>
       </v-btn>
-      <v-toolbar-title v-text="title" />
+      <v-toolbar-title class="font-weight-bold" v-text="title" />
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
+      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
         <v-icon>mdi-menu</v-icon>
       </v-btn>
     </v-app-bar>
-    <v-content>
+
+    <v-main>
       <v-container>
         <nuxt />
       </v-container>
-    </v-content>
+    </v-main>
+
     <v-navigation-drawer
       v-model="rightDrawer"
       :right="right"
       temporary
       fixed
+      class="elevation-2"
     >
       <v-list>
         <v-list-item @click.native="right = !right">
           <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
+            <v-icon light>mdi-repeat</v-icon>
           </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+          <v-list-item-content>
+            <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+          </v-list-item-content>
         </v-list-item>
-      </v-list>
+        </v-list>
     </v-navigation-drawer>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
+
+    <v-footer :fixed="fixed" app class="justify-center py-3">
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
@@ -90,33 +84,51 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
+      clipped: false, // ควบคุมว่า App Bar จะทับ Navigation Drawer หรือไม่
+      drawer: false, // ควบคุมการเปิด/ปิด Navigation Drawer ด้านซ้าย
+      fixed: false, // ควบคุมว่า Footer จะติดอยู่ด้านล่างตลอดเวลาหรือไม่
       items: [
+        // รายการเมนูสำหรับ Navigation Drawer ด้านซ้าย
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
+          icon: 'mdi-home', // เปลี่ยนไอคอนให้เหมาะสม
+          title: 'หน้าหลัก', // เปลี่ยนชื่อเมนูให้ชัดเจน
+          to: '/',
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          icon: 'mdi-lightbulb-on', // เปลี่ยนไอคอนให้เหมาะสม
+          title: 'แรงบันดาลใจ',
+          to: '/inspire',
         },
-         {
-          icon: 'mdi-apps',
+        {
+          icon: 'mdi-account-group', // เปลี่ยนไอคอนให้เหมาะสม
+          title: 'เกี่ยวกับเรา', // เปลี่ยนชื่อเมนูให้ชัดเจน
+          to: '/about', // ตัวอย่างเส้นทางใหม่
+        },
+        {
+          icon: 'mdi-star', // เพิ่มเมนูใหม่สำหรับ Nona
           title: 'Nona',
-          to: '/welcome'
-        }
+          to: '/welcome',
+        },
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      miniVariant: false, // ควบคุมโหมด Navigation Drawer แบบย่อ
+      right: true, // ควบคุมตำแหน่งของ Navigation Drawer ด้านขวา
+      rightDrawer: false, // ควบคุมการเปิด/ปิด Navigation Drawer ด้านขวา
+      title: 'Nuxtify App', // เปลี่ยนชื่อแอปพลิเคชัน
     }
-  }
+  },
 }
 </script>
+
+<style scoped>
+/*
+  สามารถเพิ่ม CSS ทั่วไปสำหรับเลย์เอาต์นี้ได้ที่นี่
+  แต่สำหรับ Vuetify มักจะใช้พร็อพพ์และคลาสที่ Vuetify มีให้มากกว่าเขียน CSS เองโดยตรง
+
+  ตัวอย่าง:
+  .v-app-bar.v-toolbar {
+    border-bottom: 2px solid var(--v-primary-lighten5);
+  }
+*/
+</style>
